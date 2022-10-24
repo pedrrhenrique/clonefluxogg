55206246742739fdf09ec3dd41d330b97758c30c
Pedro Henrique <pedrrhenriq@hotmail.com>
2022-10-24 15:20:09 -0300

    Assets criados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import logo from './assets/logo.webp'
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <img src={logo} alt='logo fluxo' />
 
+        <h1> meu nome é PEdro Henrique</h1>
+        <p>fsfifjfsd LLL lll</p>
+      </div>
     </div>
   );
 }
